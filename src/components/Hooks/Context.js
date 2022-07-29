bc63c15e6071c238df1a6e091832eef67bf0706b
unknown <sugamanandsahu01@gmail.com>
2022-07-29 09:15:48 +0530

    Adding useContext and useReducer.

--- src/components/Hooks/Context.js
+++ src/components/Hooks/Context.js
@@ -1,13 +1,33 @@
-import React, { createContext } from "react";
+import React, { createContext, useContext, useReducer } from "react";
+import { faker } from '@faker-js/faker';
+import { cartReducer } from "./Reducer";
 
 const Cart = createContext();
 
 const Context = ({ children }) => {
+
+    const products = [...Array(20)].map(() => ({
+        id: faker.datatype.uuid(),
+        name: faker.commerce.productName(),
+        price: faker.commerce.price(),
+        image: faker.image.dataUri(),
+        inStock: faker.random.numeric(),
+        fastDelivery: faker.datatype.boolean(),
+        rating: 4
+    }))
+
+    const [state, dispatch] = useReducer(cartReducer, {
+        products,
+        cart: []
+    })
+
   return (
-    <Cart.Provider>
+    <Cart.Provider value={{state, dispatch}}>
         {children}
     </Cart.Provider>
   )
 }
 
-export default Context
+export default Context;
+
+export const CartContext = () => useContext(Cart)
