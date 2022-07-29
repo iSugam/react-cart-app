f251b505f4677728482e62589fab082bebd3b415
unknown <sugamanandsahu01@gmail.com>
2022-07-29 22:44:04 +0530

    Adding SingleProduct component.

---
+++ src/components/containers/SingleProduct.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { RiShoppingCart2Fill } from "react-icons/ri"
+
+const SingleProduct = ({ product }) => {
+    console.log(product);
+    const { name, image, price, inStock, rating } = product;
+  return (
+    <div className='product-container'>
+        <div className="product-img">
+            <img src={image} alt="images" />
+        </div>
+        <span>{name}</span>
+        <span>₹{price}</span>
+        <span>Stock {inStock}</span>
+        <span>Rating {rating}</span>
+        <button className='btn btn-dark'><span><RiShoppingCart2Fill color="white" fontSize= "25px" /></span> Add to Cart</button>
+    </div>
+  )
+}
+
+export default SingleProduct
