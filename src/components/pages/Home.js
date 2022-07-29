47feb3814e77ecc921989fea4e10107c3830f4aa
unknown <sugamanandsahu01@gmail.com>
2022-07-29 09:16:43 +0530

    Importing CartContext from Context.js

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import { CartContext } from '../Hooks/Context'
 
 const Home = () => {
+  const { state } = CartContext();
+  console.log(state);
   return (
-    <div>Home</div>
+    <div>
+      Home
+    </div>
   )
 }
 
