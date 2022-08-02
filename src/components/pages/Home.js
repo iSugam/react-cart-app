8138701469fe285557b53e5e76e3f16ab17c9008
unknown <sugamanandsahu01@gmail.com>
2022-08-02 07:40:55 +0530

    Adding filteredProducts function for view only fintered products.

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -5,13 +5,54 @@ import { CartContext } from '../Hooks/Context'
 import "./home.css"
 
 const Home = () => {
-  const { state: {products} } = CartContext();
+  const { state: {products},
+  filterState: {byPrice,
+    byStock,
+    byFastDelivery,
+    byRating,
+    searchQuery,
+    sort} } = CartContext();
+
+  const filteredProducts = () => {
+    let sortedProducts = products;
+
+    // Sort by Price
+    if(sort) {
+      sortedProducts = sortedProducts.sort((a, b) => {
+        return sort === "lowToHigh"? a.price - b.price : b.price - a.price
+      })
+    }
+
+    // Sort by Include Out of Stock
+    if(!byStock) {
+      sortedProducts = sortedProducts.filter(prod => !prod.byStock)
+    }
+
+    // Sort by Fast Delivery
+    if(byFastDelivery) {
+      sortedProducts = sortedProducts.filter(prod => prod.fastDelivery)
+    }
+
+    // Sort by rating
+    if(byRating) {
+      sortedProducts = sortedProducts.filter(prod => prod.rating >= byRating)
+    }
+
+    // Sort by Search
+    if(searchQuery) {
+      sortedProducts = sortedProducts.filter(prod => {
+        return prod.name.toLowerCase().includes(searchQuery)
+      })
+    }
+
+    return sortedProducts;
+  }
   return (
     <div className="home">
           <Filter />
           <div className="all-products">
             {
-              products.map(product => {
+              filteredProducts().map(product => {
                 return <SingleProduct product = {product} key={product.id}/>
               })
             }
